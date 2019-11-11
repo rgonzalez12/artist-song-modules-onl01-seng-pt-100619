a86c1c907735807a094358c9c33b0e11f02e8e0e
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-11-11 06:11:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
-  include Memorabe::InstanceMethods
+  include Memorable::InstanceMethods
   
   
   attr_accessor :name
